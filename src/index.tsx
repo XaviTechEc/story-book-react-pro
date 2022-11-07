3505ce5bdc7ade58304a793ecb426669172abbb5
xtech <sx.guzman@hotmail.com>
2022-11-07 11:35:47 -0500

    fix: Deleting index.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,3 @@ export { Button } from './stories/Button';
 export { MyLabel, MyChip } from './components';
 export { Header } from './stories/Header';
 export { Page } from './stories/Page';
-
-import './index.css';
